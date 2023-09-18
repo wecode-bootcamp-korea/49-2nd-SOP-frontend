2d9a913e8d21077bc5d4de42d9566cf6ef148c02
jyj <dudwns5013@naver.com>
2023-09-18 20:44:12 +0900

    초기 설정

---
+++ README.md
@@ -0,0 +1,6 @@
+# 49-2nd-Team2-frontend
+
+### 🐶팀원 소개
+
+Frontend : 권순우, 박지원, 조영준
+Backend : 김정연, 다나, 박준우, 이호영
---
+++ src/Router.js
@@ -0,0 +1,14 @@
+import Main from '../src/pages/Main/Main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
