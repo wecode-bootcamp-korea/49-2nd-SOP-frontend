fad29063f7a4c817f22bd72255e37388b7310c63
jyj <dudwns5013@naver.com>
2023-09-19 13:33:38 +0900

    오타 수정2

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Signup = () => {
-  return <div>Signup</div>;
+const SignUp = () => {
+  return <div>SignUp</div>;
 };
 
-export default Signup;
+export default SignUp;
