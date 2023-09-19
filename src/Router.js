2ea2eafd00c1030ef902a400c763f61aa7adbe75
jyj <dudwns5013@naver.com>
2023-09-19 16:24:39 +0900

    헤더 고정 완료

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,11 @@
 import Main from './pages/Main/Main';
+import Header from './components/Header/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,20 @@
+import NavButton from '../NavButton/NavButton';
 import React from 'react';
+import './Header.scss';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div>
+        <NavButton to="/hair">헤어</NavButton>
+        <NavButton to="/perfume">향수</NavButton>
+      </div>
+      <div>
+        <NavButton to="/login">로그인</NavButton>
+        <NavButton to="/cart">카트</NavButton>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
