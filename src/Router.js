1dafbbf454d03d1e8c1cde464947de2a58481a5a
soonwoooo <kkshjs123@gmail.com>
2023-09-20 10:19:27 +0900

    merge전 진행상황커밋

--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,5 @@
 import Main from './pages/Main/Main';
+import Login from './pages/Login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const Router = () => {
@@ -6,6 +7,7 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import { MdClose } from 'react-icons/md';
+import './Login.scss';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <div className="loginModal">
+        {' '}
+        {/*로그인 모달페이지 */}
+        <div className="loginScreen">
+          {/*로그인 만든거 페이지 */}
+          <MdClose className="closeButton" />
+          <div className="loginContent">
+            {/*x버튼 빠진 전체 랩퍼 */}
+            <div className="loginLetter">
+              <h1>로그인</h1>
+            </div>
+            <div className="inputAndButtonWrapper">
+              {/*이메일/비밀번호/로그인 랩퍼 */}
+              <div className="emailWrapper">
+                <input className="emailInput" placeholder="이메일 주소" />
+              </div>
+              <div className="passwordWrapper">
+                <input className="passwordInput" placeholder="비밀번호" />
+                <button className="viewPassword">보기</button>
+              </div>
+              <div className="loginButtonWrapper">
+                <button className="loginButton">로그인</button>
+              </div>
+            </div>
+          </div>
+          <h2>회원이 아니신가요?</h2>
+          <p className="signupExplanation">
+            회원가입을 통해 주문 내역을 확인하고, 위시리스트에 관심제품을
+            저장하거나, 저장된 정보를 사용하여 더 빠른 온라인 결제 경험을 즐기실
+            수 있습니다
+          </p>
+          <div className="signupButtonWrapper">
+            <button className="signupButton">회원가입</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
