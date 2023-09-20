e13ca373e6f8c159b663941d3e169307d15a5316
coolllleooo <buseness200824@gmail.com>
2023-09-20 10:18:45 +0900

    헤어 UI

--- src/pages/Hair/Hair.js
+++ src/pages/Hair/Hair.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import './Hair.scss';
 
 const Hair = () => {
-  return <div>Hair</div>;
+  return (
+    <div className="HairPage">
+      <div className="HairTop">
+        <img className="esopLogo" src="/images/atom.png" alt="이솝로고" />
+        <h1 className="HairTopName">샴푸</h1>
+      </div>
+      <div className="HairMiddle">
+        <div className="box">가나다</div>
+        <div className="box">가나다</div>
+        <div className="box">가나다</div>
+        <div className="box">가나다</div>
+        <div className="box">가나다</div>
+        <div className="box">가나다</div>
+      </div>
+    </div>
+  );
 };
 
 export default Hair;
