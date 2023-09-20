01eac45bc0fe6d9d3fbc967f5f41dae57f043601
jyj <dudwns5013@naver.com>
2023-09-20 14:08:05 +0900

    feat: Footer.js 기능 개발 section 구역 UI 완료

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,22 @@
+import SectionBox from '../SectionBox/SectionBox';
+import { FOOTER_SECTION_DATA } from '../Variable';
 import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
-  return <footer />;
+  return (
+    <footer>
+      <div className="footerContainer">
+        <div className="communication">이솝 커뮤니케이션</div>
+        <div className="sectionContainer">
+          {FOOTER_SECTION_DATA.map(({ id, title, subject }) => {
+            return <SectionBox key={id} title={title} subject={subject} />;
+          })}
+        </div>
+      </div>
+      <div className="corp">© 2SOP</div>
+    </footer>
+  );
 };
 
 export default Footer;
