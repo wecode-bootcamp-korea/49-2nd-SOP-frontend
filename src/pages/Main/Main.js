7385473d1a5a8a24571709036b7b614edf5dc312
jyj <dudwns5013@naver.com>
2023-09-20 20:01:09 +0900

    feat: HeroBanner 컴포넌트화 + Product Mockdata 만들어 적용 시작

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import HeroBanner from '../../components/Main/HeroBanner/HeroBanner';
+import Product from '../../components/Main/Product/Product';
+import React, { useEffect, useState } from 'react';
 import './Main.scss';
 
 const Main = () => {
-  return <div className="main">Main</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/mainMockData.json')
+      .then(response => response.json())
+      .then(result => setData(result));
+  }, []);
+
+  if (data.length === 0) {
+    return;
+  }
+
+  return (
+    <div className="main">
+      <HeroBanner />
+
+      <div className="productSlider">
+        {data.map(info => {
+          return <Product key={info.id} info={info} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Main;
