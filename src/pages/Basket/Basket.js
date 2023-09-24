ce35eb85de08e15f374d05803dbc3cdbf3b93692
jyj <dudwns5013@naver.com>
2023-09-24 13:13:28 +0900

    fix: branch 이름 변경

--- src/pages/Basket/Basket.js
+++ src/pages/Basket/Basket.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Basket.scss';
 
 const Basket = () => {
   return (
-    <div className="Basket">
-      <div>주문 내역</div>
+    <div className="basket">
+      <img src="/images/2sop.png" alt="logo" className="logo" />
+      <div className="basketHeader">
+        <div className="order">주문 내역</div>
+        <span className="totalPrice">price</span>
+      </div>
     </div>
   );
 };
