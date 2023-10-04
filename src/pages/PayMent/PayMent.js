8a85ca8238d400b6f1de9c086b2597a1709818db
soonwoooo <kkshjs123@gmail.com>
2023-10-04 16:12:46 +0900

    docs:중간 저장

--- src/pages/PayMent/PayMent.js
+++ src/pages/PayMent/PayMent.js
@@ -2,18 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { HiChevronUp, HiChevronDown } from 'react-icons/hi2';
 import './PayMent.scss';
+import Input from '../../components/Input/Input';
 
 const PayMent = () => {
-  const [mockdata, setMockData] = useState([]);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [address, setAddress] = useState('');
+  const [detailAddress, setdetailAddress] = useState('');
+  const [productData, setProductData] = useState([]);
   const [productDetail, setProductDetail] = useState(false);
 
   useEffect(() => {
     fetch('/data/payMentData.json')
       .then(res => res.json())
       .then(data => {
-        setMockData(data);
+        setProductData(data);
       });
   }, []);
+
   const navigate = useNavigate();
   const goToMain = () => {
     navigate('/');
@@ -22,16 +29,77 @@ const PayMent = () => {
     setProductDetail(!productDetail);
   };
 
+  const saveReceiverFirstName = e => {
+    setFirstName(e.target.value);
+  };
+
+  const saveReceiverLastName = e => {
+    setLastName(e.target.value);
+  };
+
+  const saveReceiverPhoneNumber = e => {
+    setPhoneNumber(e.target.value);
+  };
+
+  const saveReceiverAddress = e => {
+    setAddress(e.target.value);
+  };
+
+  const saveReceiverDetailAddress = e => {
+    setdetailAddress(e.target.value);
+  };
+
+  const handlePayMent = event => {
+    event.preventDefault();
+    if (firstName === '' || firstName.length > 2) {
+      alert('성을 두글자 이내로 작성해주세요');
+      return;
+    } else if (lastName === '' || lastName.length > 5) {
+      alert('이름을 다섯글자 이내로 작성해주세요');
+      return;
+    } else if (
+      phoneNumber === '' ||
+      !phoneNumber.includes('-') ||
+      phoneNumber.length > 13
+    ) {
+      alert('전화번호는 (010-0000-0000) 형식으로 작성해주세요');
+      return;
+    } else if (address === '') {
+      alert('주소를 작성해주세요');
+      return;
+    } else if (detailAddress === '') {
+      alert('세부주소를 작성해주세요');
+      return;
+    }
+
+    fetch('http://10.58.52.240:8000/users/signUp', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        authorization: '토큰',
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber,
+        // nation: nation,
+        address: address,
+        detailAddress: detailAddress,
+      }),
+    });
+  };
+
   // 맵으로 돌린 부분보다 윗부분에서 총합 금액 구하는식인데
   // for (const product of mockdata) 에서 product가 의미하는것은
   // mockdata의 배열의 첫번째 요소가 product라는 변수안에 할당된다는것이다
   const totalPrice = () => {
     let totalCost = 0;
-    for (const product of mockdata) {
+    for (const product of productData) {
       totalCost += product.price * product.quantity;
     }
     return totalCost;
   };
+
   return (
     <div className="payMent">
       <div className="logoWrapper">
@@ -84,7 +152,7 @@ const PayMent = () => {
               </div>
               <div className="productDetailContainer">
                 <div>
-                  {mockdata.map(product => {
+                  {productData.map(product => {
                     const {
                       cartId,
                       productName,
@@ -129,12 +197,76 @@ const PayMent = () => {
           )}
         </div>
       </div>
-
-      <div className="orderAddressWrapper">
-        <form>
-          <div className="whereYouSendLetter">
-            <p>주문을 어디로 보내시겠습니까?</p>
+      <div className="ordererInformation">
+        <div>
+          <div>
+            <h2>주문자 정보</h2>
           </div>
+          <div>
+            <div>{productData.firstName}님</div>
+            <div>tnsdn10@naver.com</div>
+          </div>
+        </div>
+      </div>
+      <div className="sendInformationContainer">
+        <form>
+          <div className="whereSend">
+            <h1>주문을 어디로 보내시겠습니까?</h1>
+          </div>
+          <div className="formInput">
+            <div className="nameContainer">
+              <Input
+                className="firstNameInput"
+                type="text"
+                placeholder="성"
+                value={firstName}
+                onChange={saveReceiverFirstName}
+              />
+              <Input
+                className="lastNameInput"
+                type="text"
+                placeholder="이름"
+                value={lastName}
+                onChange={saveReceiverLastName}
+              />
+            </div>
+            <div className="phoneNumberContainer">
+              <Input
+                className="phoneNuberInput"
+                type="tel"
+                placeholder="전화번호(010-0000-0000)"
+                value={phoneNumber}
+                onChange={saveReceiverPhoneNumber}
+              />
+            </div>
+            <div className="nationContainer">
+              <label className="nationSelect">
+                <select>
+                  <option>대한민국</option>
+                  <option>나열되지않은 국가</option>
+                </select>
+              </label>
+            </div>
+            <div className="addressContainer">
+              <Input
+                className="addressInput"
+                type="text"
+                placeholder="주소"
+                value={address}
+                onChange={saveReceiverAddress}
+              />
+            </div>
+            <div className="detailAddresContainer">
+              <Input
+                className="detailAddressInput"
+                type="text"
+                placeholder="상세주소"
+                value={detailAddress}
+                onChange={saveReceiverDetailAddress}
+              />
+            </div>
+          </div>
+          <button onClick={handlePayMent}>결제하기로 이동</button>
         </form>
       </div>
     </div>
